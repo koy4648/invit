4de7a790c717e6e81ad2e621b1b69ec991965629
koy4648 <8.0434024e+07+koy4648@users.noreply.github.com>
2026-09-07 07:34:10 +0000

    refine mobile intro and content wrapping

--- src/components/WeddingHero.tsx
+++ src/components/WeddingHero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useGalleryPhotos } from "@/hooks/useGalleryPhotos";
 
-const OPENING_LINES = ["welcome to our wedding", "by. youngseo ♡ jinseong"];
+const OPENING_LINES = ["welcome to our", "wedding", "by. youngseo ♡ jinseong"];
 
 export default function WeddingHero() {
   const photos = useGalleryPhotos();
